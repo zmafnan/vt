85c7fb4a4c066ec7b53e3040198dc88ca2b6e19b
zmafnan <iamzafn22@gmail.com>
2026-08-22 20:15:54 +0700

    fix

--- aristotle-vendor.mk
+++ aristotle-vendor.mk
@@ -963,7 +963,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.core-impl-mediatek \
     android.hardware.bluetooth.audio-impl-mediatek \
     android.hardware.power-service-mediatek \
-    prebuilt_android.hardware.graphics.common-V6-ndk \
     com.fingerprints.extension3-V1-ndk \
     libGLES_meow \
     libMEOW_data \
